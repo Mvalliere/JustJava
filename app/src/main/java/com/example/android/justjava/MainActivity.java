aaec68ba200502246ce226032989a3e7b9410a3d
mark valliere <mark valliere>
2015-12-27 14:12:21 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends ActionBarActivity {
+
+    int quantity = 0;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+
+        int price = quantity * 5;
+
+        String priceMessage = price + " dollars for " + quantity + " cups of coffee. Pay up.";
+        priceMessage = priceMessage + "\nThanks dawg!";
+        displayMessage(priceMessage);
+
+
+
+    }
+
+
+    public void incrementQuantity(View view) {
+        displayPrice(0);
+        quantity = quantity + 1;
+        display(quantity);
+    }
+
+
+    public void decrementQuantity(View view) {
+        displayPrice(0);
+        quantity = quantity - 1;
+
+        if (quantity >= 0) {
+            display(quantity);
+        }
+        else{
+            display(0);
+            quantity = 0;
+        }
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
+    }
+}
